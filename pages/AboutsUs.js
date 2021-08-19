8e3f665f099a280f589fbe2a34eade9bd2cec503
Creole Desktop <jignesh@7span.com>
2021-08-19 16:34:45 +0530

    4th time

--- pages/AboutsUs.js
+++ pages/AboutsUs.js
@@ -2,13 +2,13 @@ import Layout from "../component/Layout"
 
 const AboutUs = ({ title, description, ...props }) => {
     return (
-        <div className="AboutUs_Page">
+        <>
             <Layout pageTitle={title} description={description}></Layout>
             <h1 className="title">Content of about us Page</h1>
             <p>
                 I am a very exciting person.
             </p>
-        </div>
+        </>
     )
 }
 export default AboutUs;
