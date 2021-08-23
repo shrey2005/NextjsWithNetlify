a1fc35d99b1b0e8fb439f970220623ed69afc4a6
Creole Desktop <jignesh@7span.com>
2021-08-23 17:04:10 +0530

    Login Component Created!

--- pages/Login.js
+++ pages/Login.js
@@ -1,30 +1,11 @@
 import Layout from "../component/Layout"
+import LoginForm from "../component/LoginForm";
 
 const Login = () => {
     return (
         <div className="Login_Page">
             <Layout pageTitle="Login Page"></Layout>
-            <h1 className="title">Login Page</h1>
-            <form name="simpleContactForm" method="POST" data-netlify="true" id="simple-contact-form" className="contact-form">
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <label>Your Role: <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                    </select></label>
-                </p>
-                <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
-            </form>
+            <LoginForm></LoginForm>
         </div>
     );
 }
