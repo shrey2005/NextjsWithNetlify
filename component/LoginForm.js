009ac2097bbc9ace9caae5dd9905f9c99182187a
Creole Desktop <jignesh@7span.com>
2021-08-23 17:40:44 +0530

    Label Changes in HTML

--- component/LoginForm.js
+++ component/LoginForm.js
@@ -2,25 +2,24 @@ export default function ContactForm() {
     return (
         <form name="contact" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
-            <p>
+            <div className="Name">
+                <label htmlFor="firstname">
+                    Name:
+                </label>
                 <input type="text" name="firstname" id="firstname" />
-                <label htmlFor="yourname">
-                    Your Name:
-                </label> <br />
-                <input type="text" name="name" id="yourname" />
-            </p>
-            <p>
-                <label htmlFor="youremail">
-                    Your Email:
-                </label> <br />
-                <input type="email" name="email" id="youremail" />
-            </p>
-            <p>
-                <label htmlFor="yourmessage">
+            </div>
+            <div className="Email">
+                <label htmlFor="email">
+                    Email:
+                </label>
+                <input type="email" name="email" id="email" />
+            </div>
+            <div>
+                <label htmlFor="Message">
                     Message:
-                </label> <br />
-                <textarea name="message" id="yourmessage"></textarea>
-            </p>
+                </label>
+                <textarea name="message" id="Message"></textarea>
+            </div>
             <p>
                 <button type="submit">Send</button>
             </p>
